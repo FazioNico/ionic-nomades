32e67e8beb42814baa7d97c4ebba341baf47ccd1
FazioNico <contact@nicolasfazio.ch>
2017-10-23 11:36:05 +0200

    rmv native plugins

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,8 @@
 
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+// import { StatusBar } from '@ionic-native/status-bar';
+// import { SplashScreen } from '@ionic-native/splash-screen';
 
 @Component({
   templateUrl: 'app.html'
@@ -17,12 +17,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   rootPage:string;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(
+    platform: Platform,
+    // statusBar: StatusBar,
+    // splashScreen: SplashScreen
+  ) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
+      // statusBar.styleDefault();
+      // splashScreen.hide();
       if(platform.is('core') /*|| platform.is('mobileweb')*/){
         this.rootPage = 'BrowserPage';
       }
