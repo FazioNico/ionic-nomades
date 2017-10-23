1dcf552aad5dec72d27cf077e83c9a81bf70e1d9
FazioNico <contact@nicolasfazio.ch>
2017-10-23 15:05:00 +0200

    rmv sw cause prob with lazyloading

--- src/app/main.ts
+++ src/app/main.ts
@@ -13,13 +13,13 @@ import { AppModule } from './app.module';
 platformBrowserDynamic()
 .bootstrapModule(AppModule)
 .then(_=> {
-  let cond = 'serviceWorker' in navigator && ['localhost', '127', '8100'].indexOf(location.hostname) === -1;
-  if (cond) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('service worker installed'))
-    .catch(err => console.error('Error', err));
-  }
-  else {
-    console.log('No service worker:', cond)
-  }
+  // let cond = 'serviceWorker' in navigator && ['localhost', '127', '8100'].indexOf(location.hostname) === -1;
+  // if (cond) {
+  //   navigator.serviceWorker.register('service-worker.js')
+  //   .then(() => console.log('service worker installed with success!'))
+  //   .catch(err => console.error('Error', err));
+  // }
+  // else {
+  //   console.log('No service worker:', cond)
+  // }
 })
