910e4b6a0ade65a95348cbce03d2f4402c055380
FazioNico <contact@nicolasfazio.ch>
2017-10-24 11:25:37 +0200

    fix style...

--- src/pages/mobile/contact/contact.ts
+++ src/pages/mobile/contact/contact.ts
@@ -3,7 +3,7 @@
  * @Date:   16-10-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 20-10-2017
+ * @Last modified time: 24-10-2017
  */
 
 import { Component } from '@angular/core';
