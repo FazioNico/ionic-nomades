b9e37eb7a305f0ef95bc5af46fe26966d5d84c64
FazioNico <contact@nicolasfazio.ch>
2017-10-30 12:05:53 +0100

    enable service worker

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ $ npm install
 $ ionic serve
 ```
 
+## Google Audits Score
+![PWA Score](https://raw.githubusercontent.com/FazioNico/ionic-nomades/master/google-audits-pwa.jpg)
+
+
 ## About author
 
 Hi, i'm a Front-end developper living in Geneva Switzerland and i build hybrid mobile & web applications for almost 15 years. You can follow me on Twitter @FazioNico or checkout my own website http://nicolasfazio.ch
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,7 @@
  * @Date:   08-09-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 23-10-2017
+ * @Last modified time: 30-10-2017
  */
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
@@ -13,13 +13,13 @@ import { AppModule } from './app.module';
 platformBrowserDynamic()
 .bootstrapModule(AppModule)
 .then(_=> {
-  // let cond = 'serviceWorker' in navigator && ['localhost', '127', '8100'].indexOf(location.hostname) === -1;
-  // if (cond) {
-  //   navigator.serviceWorker.register('service-worker.js')
-  //   .then(() => console.log('service worker installed with success!'))
-  //   .catch(err => console.error('Error', err));
-  // }
-  // else {
-  //   console.log('No service worker:', cond)
-  // }
+  let cond = 'serviceWorker' in navigator && ['localhost', '127', '8100'].indexOf(location.hostname) === -1;
+  if (cond) {
+    navigator.serviceWorker.register('service-worker.js')
+    .then(() => console.log('service worker installed with success!'))
+    .catch(err => console.error('Error', err));
+  }
+  else {
+    console.log('No service worker:', cond)
+  }
 })
