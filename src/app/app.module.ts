a22663e6bf9053b4a05def035e212de61c35a810
FazioNico <contact@nicolasfazio.ch>
2017-11-02 11:00:26 +0100

    fix service worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@
  * @Date:   30-05-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 11-10-2017
+ * @Last modified time: 02-11-2017
  */
 
 import { BrowserModule } from '@angular/platform-browser';
--- src/app/main.ts
+++ src/app/main.ts
@@ -3,23 +3,48 @@
  * @Date:   08-09-2017
  * @Email:  contact@nicolasfazio.ch
  * @Last modified by:   webmaster-fazio
- * @Last modified time: 30-10-2017
+ * @Last modified time: 02-11-2017
  */
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
 
-platformBrowserDynamic()
-.bootstrapModule(AppModule)
-.then(_=> {
+export interface WindowSW extends Window {
+  toto:any;
+}
+
+//AppModule.installServiceWorker();
+platformBrowserDynamic().bootstrapModule(AppModule).then(_=> {
   let cond = 'serviceWorker' in navigator && ['localhost', '127', '8100'].indexOf(location.hostname) === -1;
   if (cond) {
-    navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log('service worker installed with success!'))
-    .catch(err => console.error('Error', err));
+      navigator.serviceWorker.register('service-worker.js')
+      .then((registration) => {
+        console.log('[serviceWorker] installed with success!')
+        registration.onupdatefound = () => {
+          const installingWorker = registration.installing;
+          if(!installingWorker) {
+            return;
+          }
+
+          installingWorker.onstatechange = () => {
+            console.log('workerState->',installingWorker.state)
+            switch (installingWorker.state) {
+              case 'installed':
+                if (navigator.serviceWorker.controller) {
+                  console.log('[serviceWorker] new update available')
+                } else {
+                  console.log('[serviceWorker] no update available')
+                }
+                break;
+            }
+          };
+        };
+      })
+      .catch(err => console.error('Error', err));
+
   }
   else {
-    console.log('No service worker:', cond)
+    console.log('[serviceWorker] no service worker available. status:', cond)
   }
 })
